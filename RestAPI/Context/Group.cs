68269dc8bc46325965d6c4c8b9ff79966bb1a198
OneZeroBeatz <onezerobeatz@gmail.com>
2021-04-29 13:23:40 +0200

    [RestApi] - Project and database entities added

---
+++ RestAPI/Context/Group.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RestAPI.Context
+{
+    public class Group
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        [ForeignKey("GroupId")]
+        public ICollection<User> Students { get; set; }
+    }
+}
---
+++ RestAPI/Context/RestApiDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RestAPI.Context
+{
+    public class RestApiDbContext : DbContext
+    {
+        public RestApiDbContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        DbSet<User> Users { get; set; }
+        DbSet<Group> Groups { get; set; }
+    }
+}
